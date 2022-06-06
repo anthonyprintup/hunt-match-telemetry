651a1efeb799cd9c39ec761fa4826cbe7f221748
Anthony Printup <anthony@printup.io>
2022-06-06 13:56:53 +0200

    Moved formatting related constants to constants.py, added comments for documentation.

--- src/hunt/constants.py
+++ src/hunt/constants.py
@@ -1,5 +1,11 @@
 import os.path
 
-HUNT_SHOWDOWN_STEAM_ID: int = 594650
-USER_PROFILE_ID: int = 0
+HUNT_SHOWDOWN_STEAM_ID: int = 594650  # Hunt: Showdown team ID (to locate the game's install path)
+USER_PROFILE_ID: int = 0  # Internal Crytek identifier (grabbed from the attributes file)
+
+# The path (directory) to where match logs are stored
 RESOURCES_PATH: str = os.path.realpath(os.path.join(os.path.dirname(__file__), r"../../resources"))
+
+# Formatting
+STAR_SYMBOL: str = "★"
+MMR_RANGES: tuple[int, ...] = (0, 2000, 2300, 2600, 2750, 3000)
