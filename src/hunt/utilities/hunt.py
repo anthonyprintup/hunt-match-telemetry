2e8e4bcdda63c0edbcd90dfc1af0f572a8425507
Anthony Printup <anthony@printup.io>
2022-06-07 21:24:23 +0200

    Refactored code, added a function to fetch the Steam username: - renamed find_hunt_attributes_path to fetch_hunt_attributes_path, - added exception handling to fetch_hunt_attributes_path, - optimized fetch_hunt_attributes_path (no longer iterates through all the keys in the "Uninstall" path) - added a function to fetch the user's Steam username to avoid using the Crytek profile id.

--- src/hunt/utilities/hunt.py
+++ src/hunt/utilities/hunt.py
@@ -1,29 +1,40 @@
-from winreg import HKEYType, HKEY_LOCAL_MACHINE, OpenKey, QueryInfoKey, EnumKey, QueryValueEx
+from winreg import HKEYType, HKEY_LOCAL_MACHINE, HKEY_CURRENT_USER, OpenKey, QueryValueEx
 from colorama import Fore, Style
 
 from ..constants import HUNT_SHOWDOWN_STEAM_ID, MMR_RANGES, STAR_SYMBOL
 
 
-def find_hunt_attributes_path() -> str:
+def fetch_hunt_attributes_path() -> str:
     """
     Locates the game's install path and appends the attributes file path to it.
     :return: A path to the attributes file
     :throws: RuntimeError if the attributes file cannot be found.
     """
-    uninstall_key: HKEYType
-    with OpenKey(key=HKEY_LOCAL_MACHINE,
-                 sub_key=r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall") as uninstall_key:
-        sub_key_count: int
-        [sub_key_count, _, _] = QueryInfoKey(uninstall_key)
-        for i in range(sub_key_count):
-            sub_key_name: str = EnumKey(uninstall_key, i)
-            if sub_key_name == f"Steam App {HUNT_SHOWDOWN_STEAM_ID}":
-                steam_app_key: HKEYType
-                with OpenKey(key=uninstall_key, sub_key=sub_key_name) as steam_app_key:
-                    install_location: str
-                    [install_location, _] = QueryValueEx(steam_app_key, "InstallLocation")
-                    return install_location + r"\user\profiles\default\attributes.xml"
-    raise RuntimeError("Couldn't find the attributes file.")
+    registry_location: str = fr"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App {HUNT_SHOWDOWN_STEAM_ID}"
+    try:
+        steam_app_key: HKEYType
+        with OpenKey(key=HKEY_LOCAL_MACHINE, sub_key=registry_location) as steam_app_key:
+            install_location: str
+            [install_location, _] = QueryValueEx(steam_app_key, "InstallLocation")
+            return install_location + r"\user\profiles\default\attributes.xml"
+    except FileNotFoundError:
+        raise RuntimeError("Couldn't find the attributes file.")
+
+
+def fetch_steam_username() -> str:
+    """
+    Gets the user's Steam username.
+    :return: The last game username used by Steam.
+    :throws: RuntimeError if the username cannot be located.
+    """
+    try:
+        steam_key: HKEYType
+        with OpenKey(key=HKEY_CURRENT_USER, sub_key=r"SOFTWARE\Valve\Steam") as steam_key:
+            last_game_named_used: str
+            [last_game_named_used, _] = QueryValueEx(steam_key, "LastGameNameUsed")
+            return last_game_named_used
+    except FileNotFoundError:
+        raise RuntimeError("Failed to find Steam's last used username.")
 
 
 def format_mmr(mmr: int) -> str:
