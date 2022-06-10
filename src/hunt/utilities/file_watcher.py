66a6b81ab0e20456dbd0c41c35e4967cf3249de2
Anthony Printup <anthony@printup.io>
2022-06-10 17:20:14 +0200

    Added docstrings to several FileWatchdog methods.

--- src/hunt/utilities/file_watcher.py
+++ src/hunt/utilities/file_watcher.py
@@ -17,15 +17,19 @@ class FileWatchdog(FileSystemEventHandler):
         self._observer.schedule(event_handler=self, path=os.path.dirname(file_path))
 
     def start(self):
+        """Start the observer."""
         self._observer.start()
 
     def join(self):
+        """Wait until the observer thread terminates."""
         self._observer.join()
 
     def stop(self):
+        """Stop the observer."""
         self._observer.stop()
 
     def on_modified(self, event: FileSystemEvent):
+        """Invoked when a modified event is generated."""
         if not isinstance(event, FileModifiedEvent) or os.path.realpath(event.src_path) != self.file_path:
             return
         self.callback(self.file_path)
