4c2a4b9924d3b2572cd07e6fdd85d28a49f3f74b
Anthony Printup <anthony@printup.io>
2022-06-29 15:24:20 +0200

    Added new member variables to the `Rewards` class.

--- src/hunt/attributes/rewards.py
+++ src/hunt/attributes/rewards.py
@@ -4,5 +4,9 @@ from dataclasses import dataclass
 @dataclass(frozen=True)
 class Rewards:
     bounty: int
+    xp: int
     hunt_dollars: int
+    bloodbonds: int
     hunter_xp: int
+    upgrade_points: int
+    bloodline_xp: int
