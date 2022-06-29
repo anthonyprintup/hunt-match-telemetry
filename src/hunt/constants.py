cfa6eaacc76746a60e08fe63d95e52165712ead4
Anthony Printup <anthony@printup.io>
2022-06-29 15:02:46 +0200

    Changed the `name` column's type to `TEXT` in the `player_log` tables.

--- src/hunt/constants.py
+++ src/hunt/constants.py
@@ -27,7 +27,7 @@ def create_table_helper(table_name: str, fields: tuple[str, ...]) -> str:
 
 # Tables
 _PLAYER_LOG_COLUMNS = ("id INTEGER PRIMARY KEY",
-                       "profile_id INTEGER UNIQUE", "name varchar(32) NOT NULL", "mmr INTEGER DEFAULT 0 NOT NULL",
+                       "profile_id INTEGER UNIQUE", "name TEXT NOT NULL", "mmr INTEGER DEFAULT 0 NOT NULL",
                        "kills INTEGER DEFAULT 0 NOT NULL", "deaths INTEGER DEFAULT 0 NOT NULL",
                        "encounters INTEGER DEFAULT 0 NOT NULL")
 DATABASE_TABLE_QUERIES: tuple[str, ...] = (
