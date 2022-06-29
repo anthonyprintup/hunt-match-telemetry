d13c4e92a490cb178153defbe949c4b629d2a2e4
Anthony Printup <anthony@printup.io>
2022-06-29 14:44:59 +0200

    Refactored player log tables: - seperated table columns to prevent duplicate code, - the player log is now split into two tables: `player_log_bountyhunt` and `player_log_quickplay` for each game mode respectively.

--- src/hunt/constants.py
+++ src/hunt/constants.py
@@ -26,11 +26,12 @@ def create_table_helper(table_name: str, fields: tuple[str, ...]) -> str:
 
 
 # Tables
+_PLAYER_LOG_COLUMNS = ("id INTEGER PRIMARY KEY",
+                       "profile_id INTEGER UNIQUE", "name varchar(32) NOT NULL", "mmr INTEGER DEFAULT 0 NOT NULL",
+                       "kills INTEGER DEFAULT 0 NOT NULL", "deaths INTEGER DEFAULT 0 NOT NULL",
+                       "encounters INTEGER DEFAULT 0 NOT NULL")
 DATABASE_TABLE_QUERIES: tuple[str, ...] = (
     create_table_helper("data_hashes", ("id INTEGER PRIMARY KEY", "hash varchar(64) UNIQUE", "path TEXT NOT NULL")),
-    create_table_helper("player_log", ("id INTEGER PRIMARY KEY", "profile_id INTEGER UNIQUE",
-                                       "latest_name varchar(32) NOT NULL", "latest_mmr INTEGER DEFAULT 0 NOT NULL",
-                                       "times_killed INTEGER DEFAULT 0 NOT NULL",
-                                       "times_died INTEGER DEFAULT 0 NOT NULL",
-                                       "times_seen INTEGER DEFAULT 0 NOT NULL"))
+    create_table_helper("player_log_bountyhunt", _PLAYER_LOG_COLUMNS),
+    create_table_helper("player_log_quickplay", _PLAYER_LOG_COLUMNS)
 )
