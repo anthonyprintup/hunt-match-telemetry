a16ac1a1b085b3f52f0973500d93f76e090457f1
Anthony Printup <anthony@printup.io>
2022-07-01 19:25:11 +0200

    Refactored the `Entry` class: - because the `Entry` and `Accolade` classes are identical, `Entry` can inherit from `Accolade`.

--- src/hunt/attributes/entry.py
+++ src/hunt/attributes/entry.py
@@ -1,12 +1,6 @@
-from dataclasses import dataclass
+from accolade import Accolade, dataclass
 
 
 @dataclass(frozen=True)
-class Entry:
-    amount: int
-    category: str
-    descriptor_name: str
-    descriptor_score: int
-    descriptor_type: int
-    reward_type: int
-    reward_size: int
+class Entry(Accolade):
+    pass
