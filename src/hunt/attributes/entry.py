94761b2f779ce8437d647fae95fd0e986196e6d2
Anthony Printup <anthony@printup.io>
2022-07-01 20:21:07 +0200

    Fixed the `Entry` class: - `Entry` and `Accolade` do not have identical member variables.

--- src/hunt/attributes/entry.py
+++ src/hunt/attributes/entry.py
@@ -1,6 +1,12 @@
-from .accolade import Accolade, dataclass
+from dataclasses import dataclass
 
 
 @dataclass(frozen=True)
-class Entry(Accolade):
-    pass
+class Entry:
+    amount: int
+    category: str
+    descriptor_name: str
+    descriptor_score: int
+    descriptor_type: int
+    reward_type: int
+    reward_size: int
