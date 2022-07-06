e1d3106d8ee0184b17f917e033563c6e301fd58a
Anthony Printup <anthony@printup.io>
2022-07-06 05:21:13 +0200

    Added a Player class for the upcoming patch (1.9).

--- src/hunt/attributes/player.py
+++ src/hunt/attributes/player.py
@@ -12,3 +12,25 @@ class Player:
     profile_id: int
     used_proximity_chat: bool
     is_skillbased: bool
+
+
+@dataclass(frozen=True)
+class TestServerPlayer:
+    name: str
+    bounties_extracted: int
+    bounties_picked_up: int
+    downed_by_me: int
+    downed_by_teammate: int
+    downed_me: int
+    downed_teammate: int
+    had_wellspring: bool
+    is_partner: bool
+    is_soul_survivor: bool
+    killed_by_me: int
+    killed_by_teammate: int
+    mmr: int
+    profile_id: int
+    proximity_to_me: bool
+    proximity_to_teammate: bool
+    skillbased: bool
+    team_extraction: bool
