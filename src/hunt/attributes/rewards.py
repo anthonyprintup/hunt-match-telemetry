c1105628d737546d7621b4720805dcf1cc8f83e6
Anthony Printup <anthony@printup.io>
2022-07-10 09:23:01 +0200

    Added a bool override for the `Rewards` class.

--- src/hunt/attributes/rewards.py
+++ src/hunt/attributes/rewards.py
@@ -11,3 +11,10 @@ class Rewards:
     hunter_levels: int
     upgrade_points: int
     bloodline_xp: int
+
+    def __bool__(self) -> bool:
+        """
+        Override the bool method to check if there were any rewards.
+        :return: True if the match had rewards otherwise False
+        """
+        return any(variable_value for variable_value in vars(self).values())
