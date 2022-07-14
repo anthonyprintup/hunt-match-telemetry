2c1482e04e952dc0988a3f3b2aa4b2969650b2b6
Anthony Printup <anthony@printup.io>
2022-07-14 12:08:04 +0200

    Added format helpers to `Player` to avoid duplicate code.

--- src/hunt/attributes/player.py
+++ src/hunt/attributes/player.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass
 
+from colorama import Fore, Style
+
+from ..formats import format_mmr
+
 
 @dataclass(frozen=True)
 class Player:
@@ -24,3 +28,29 @@ class Player:
     proximity_to_teammate: bool
     skillbased: bool
     team_extraction: bool
+
+    def format_name(self, color_prefix: str = Fore.GREEN, is_local_player: bool = False) -> str:
+        """
+        Generate a formatted name.
+        :param color_prefix: the color to prefix the name with
+        :param is_local_player: the Player instance is the instance of the local player
+        :return: a color formatted string
+        """
+        local_player_marker: str = " (you)" if is_local_player else ""
+        return f"{color_prefix}{self.name} ({format_mmr(self.mmr)}){Style.RESET_ALL}{local_player_marker}"
+
+    def format_kills(self) -> str:
+        """
+        Generate a formatted name with the amount of times this player was killed by the local player.
+        :return: a color formatted string
+        """
+        kill_count: str = f" {self.killed_by_me}x" if self.killed_by_me > 1 else ""
+        return f"Killed {self.format_name()}{kill_count}"
+
+    def format_deaths(self) -> str:
+        """
+        Generate a formatted name with the amount of times this player killed the local player.
+        :return: a color formatted string
+        """
+        death_count: str = f" {self.killed_me}x" if self.killed_me > 1 else ""
+        return f"Died to {self.format_name(color_prefix=Fore.RED)}{death_count}"
