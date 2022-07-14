6e3b1a1102d57b77201b577ab3a33ea0da0a008e
Anthony Printup <anthony@printup.io>
2022-07-14 22:33:30 +0200

    Added missing docstrings to `test_parse_match`.

--- tests/attributes/test_parser.py
+++ tests/attributes/test_parser.py
@@ -4,4 +4,9 @@ from hunt.attributes.parser import parse_match, Match
 
 
 def test_parse_match(attributes_tree: XmlElement, expected_match: Match) -> None:
+    """
+    Test parse_match by comparing the parsed Match instance to the expected Match instance.
+    :param attributes_tree: a generated element tree
+    :param expected_match: the Match instance to compare against
+    """
     assert parse_match(attributes_tree, steam_name=expected_match.player_name) == expected_match
