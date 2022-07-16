a5349f5f0a2b3226dfd774021ebedc0e007bfa47
Anthony Printup <anthony@printup.io>
2022-07-16 07:23:53 +0200

    Implemented the `Serializable` class interface: - the `Serializable` metaclass forces its subclasses to define 3 methods (`_data_mapping`, `serialize`, and `deserialize`) which are usually overloaded to add custom support for data types based on how they're encoded in the attributes file, - the `_data_mappings` method provides mappings between local class variables and attribute name suffixes (prefixes are given to `serialize` and `deserialize`), - the `serialize` method is responsible for appending new elements (variables are fetched from the mappings) to the root element, - the `deserialize` method is responsible for parsing a series of elements into a subclass instance.

--- src/hunt/attributes/xml/serializable.py
+++ src/hunt/attributes/xml/serializable.py
@@ -1,23 +1,49 @@
-from __future__ import annotations
-
+import builtins
 from abc import ABC, abstractmethod
-from xml.etree.ElementTree import Element as XmlElement
+from dataclasses import is_dataclass
+from typing import TypeVar, Generator, TypeAlias
+
+from .elements import XmlElement, ValueType as ElementValueType, append_element, get_element_value
+
+_T = TypeVar("_T", bound="Serializable")
+MappingGenerator: TypeAlias = Generator[tuple[str, str], None, None]
 
 
 class Serializable(ABC):
+    @staticmethod
     @abstractmethod
-    def serialize(self, root: XmlElement) -> None:
+    def _data_mappings() -> MappingGenerator:
+        raise NotImplementedError("Unimplemented _data_mappings method.")
+
+    @abstractmethod
+    def serialize(self, root: XmlElement, name_prefix: str) -> None:
         """
         Serialize the current class into a series of XmlElement instances.
         :param root: the root element to serialize into
+        :param name_prefix: the name prefix to use when appending elements
         """
-        raise NotImplementedError("Unimplemented serialize method.")
+        assert is_dataclass(self.__class__), "The class should be a dataclass."
 
-    @staticmethod
+        # Append each element
+        for variable_name, name_suffix in self._data_mappings():
+            append_element(root, name=f"{name_prefix}_{name_suffix}", value=self.__getattribute__(variable_name))
+
+    @classmethod
     @abstractmethod
-    def deserialize(root: XmlElement) -> Serializable:
+    def deserialize(cls: type[_T], root: XmlElement, name_prefix: str) -> _T:
         """
         Deserialize a series of elements into the class instance.
         :param root: the root element to deserialize from
+        :param name_prefix: the name prefix to use when resolving elements
         """
-        raise NotImplementedError("Unimplemented deserialize method.")
+        assert is_dataclass(cls), "The class should be a dataclass."
+
+        # Generate the data by fetching each element value
+        annotations: dict[str, str] = cls.__annotations__
+        data: dict[str, ElementValueType] = dict(
+            (variable_name, get_element_value(root, name=f"{name_prefix}_{name_suffix}",
+                                              result_type=getattr(builtins, annotations[variable_name])))
+            for variable_name, name_suffix in cls._data_mappings())
+
+        # Construct and return the class
+        return cls(**data)
