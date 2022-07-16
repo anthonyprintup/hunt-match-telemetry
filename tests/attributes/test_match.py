7380af6964dd9c232bcd898ecc24e800ad0e9e08
Anthony Printup <anthony@printup.io>
2022-07-16 14:40:46 +0200

    Added tests for `Match.try_save_to_file`.

--- tests/attributes/test_match.py
+++ tests/attributes/test_match.py
@@ -1,6 +1,8 @@
-from datetime import datetime
+import json
 
-from hunt.attributes.match import Match
+from hunt.attributes.match import Match, DatabaseClient
+
+from .conftest import MAGIC_FILE_PATH, datetime, MagicMock
 
 
 def test_match_generate_file_path(expected_match: Match, static_time: datetime, expected_file_path: str) -> None:
@@ -12,3 +14,23 @@ def test_match_generate_file_path(expected_match: Match, static_time: datetime, 
     """
     assert expected_match.generate_file_path(time=static_time) == expected_file_path
     assert expected_match.generate_file_path() != expected_file_path
+
+
+def test_match_try_save_to_file(io_safe_match: Match, database_client: DatabaseClient, mock_open: MagicMock) -> None:
+    """
+    Test Match.try_save_to_file using wrapped Match instance to prevent filesystem access.
+    :param io_safe_match: an IO safe Match instance
+    :param database_client: a Database instance
+    :param mock_open: the MagicMock instance used to patch builtins.open
+    """
+    # Invoke Match methods
+    assert io_safe_match.generate_file_path() == MAGIC_FILE_PATH
+    assert not io_safe_match.try_save_to_file(database=database_client)  # hash doesn't exist, inserted match data
+    assert io_safe_match.try_save_to_file(database=database_client)  # hash already exists
+
+    # Assertions
+    match_data: str = json.dumps(io_safe_match, indent=2, default=vars)
+    mock_open.assert_called_once_with(MAGIC_FILE_PATH, mode="w")  # assert open(MAGIC_FILE_PATH, mode="w") invoked
+
+    mock_open_handle: MagicMock = mock_open()
+    mock_open_handle.write.assert_called_once_with(match_data)  # assert file.write(match_data) invoked
